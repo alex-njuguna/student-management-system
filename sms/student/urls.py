e6d33878fa690a63bb789a8200b3664d8168ea53
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-13 14:58:45 +0300

    add student functionality and routing

--- sms/student/urls.py
+++ sms/student/urls.py
@@ -8,4 +8,5 @@ app_name = "student"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("add_student/", views.add_student, name="add_student"),
 ]
--- sms/student/views.py
+++ sms/student/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import Student
+from .forms import AddStudentForm
 
 # display all students
 def home(request):
@@ -9,4 +11,28 @@ def home(request):
     return render(request, "student/home.html", {
         "students": students,
         "title": "home"
+    })
+
+
+# add a student
+def add_student(request):
+    form = AddStudentForm()
+
+    if request.method == "POST":
+        form = AddStudentForm(request.POST)
+        
+        if form.is_valid():
+            first_name = form.cleaned_data.get("first_name")
+            last_name = form.cleaned_data.get("last_name")
+            form.save()
+            messages.success(request, f"Student: {first_name} {last_name} saved.")
+
+            return redirect("student:home")
+        else:
+            messages.error(request, "Incorrect Details")
+            form = AddStudentForm()
+
+    return render(request, "student/add_student.html", {
+        "form": form,
+        "title": "add-student",
     })
