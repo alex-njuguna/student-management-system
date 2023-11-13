e67a73132ef0dea2c1ca884be642afdf4a6de3e2
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-13 14:59:41 +0300

    database modifications

--- sms/student/forms.py
+++ sms/student/forms.py
@@ -6,12 +6,13 @@ from .models import Student
 class AddStudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ["reg_no", "first_name", "last_name", "course", "gpa"]
+        fields = ["reg_no", "first_name", "last_name", "email", "course", "gpa"]
 
         labels = {
             "reg_no": "Registration Number: ", 
             "first_name": "First Name: ", 
             "last_name": "Last Name: ", 
+            "email": "Email",
             "course": "Course: ", 
             "gpa": "GPA: "
         }
@@ -20,6 +21,7 @@ class AddStudentForm(forms.ModelForm):
             "reg_no": forms.NumberInput(attrs={"class": "form-control"}), 
             "first_name": forms.TextInput(attrs={"class": "form-control"}), 
             "last_name": forms.TextInput(attrs={"class": "form-control"}), 
+            "email": forms.EmailInput(attrs={"class": "form-control"}), 
             "course": forms.TextInput(attrs={"class": "form-control"}), 
             "gpa": forms.NumberInput(attrs={"class": "form-control"})
         }
--- sms/student/models.py
+++ sms/student/models.py
@@ -5,6 +5,7 @@ class Student(models.Model):
     reg_no = models.PositiveIntegerField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField()
     course = models.CharField(max_length=50)
     gpa = models.FloatField()
 
