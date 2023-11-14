9d27acfc2d9957b7cb0c62307c0d5a8e33d8ab9b
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-14 04:39:31 +0300

    update student templating

--- sms/student/urls.py
+++ sms/student/urls.py
@@ -9,6 +9,6 @@ app_name = "student"
 urlpatterns = [
     path("", views.home, name="home"),
     path("add_student/", views.add_student, name="add_student"),
-    path("update_student/", views.update_student, name="update_student"),
-    path("delete_student/", views.delete_student, name="delete_student"),
+    path("update_student/<int:id>", views.update_student, name="update_student"),
+    path("delete_student/<int:id>", views.delete_student, name="delete_student"),
 ]
